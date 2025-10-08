28bb936fece624e634b6a2f75bc0b7be0c8dd336
slixenfeld <slixenfeld@mailbox.org>
2025-10-08 18:03:13 +0200

    fix indentation

--- stopwatch_dialog.asm
+++ stopwatch_dialog.asm
@@ -2,15 +2,14 @@
 
         format PE GUI 4.0
         include 'win32wx.inc'
-
-        ID_STATIC= 50
-        ID_START = 200
-        ID_STOP  = 201
+; constants
+        ID_STATIC       =       50
+        ID_START        =       200
+        ID_STOP         =       201
 .data
 
 ; handle
         hwnd           dd       ?
-
 
 ; timer
         time_text      rb       0xFF
@@ -24,107 +23,107 @@
 .code
 
 start:
-                invoke  GetModuleHandle,0
+        invoke  GetModuleHandle,0
 
-                invoke  DialogBoxParam,         \
-                        eax,                    \
-                        123,                    \
-                        HWND_DESKTOP,           \
-                        DialogProc,             \
-                        0
+        invoke  DialogBoxParam,         \
+                eax,                    \
+                123,                    \
+                HWND_DESKTOP,           \
+                DialogProc,             \
+                0
 
-                invoke  ExitProcess,0
+        invoke  ExitProcess,0
 ThreadProc:
-                push    ebp
-                mov     ebp,esp
-                mov     [second],-1
-                mov     [minute],0
-                mov     [hour],0
+        push    ebp
+        mov     ebp,esp
+        mov     [second],-1
+        mov     [minute],0
+        mov     [hour],0
   .loop:
-                inc     [second]
-                cmp     [second],60
-                jne     .skip
-                mov     [second],0
-                inc     [minute]
-                cmp     [minute],60
-                jne     .skip
-                mov     [minute],0
-                inc     [hour]
+        inc     [second]
+        cmp     [second],60
+        jne     .skip
+        mov     [second],0
+        inc     [minute]
+        cmp     [minute],60
+        jne     .skip
+        mov     [minute],0
+        inc     [hour]
   .skip:
-                invoke  wsprintf,               \
-                        time_text,              \
-                        '%02i:%02i:%02i',       \
-                        [hour],                 \
-                        [minute],               \
-                        [second]
+        invoke  wsprintf,               \
+                time_text,              \
+                '%02i:%02i:%02i',       \
+                [hour],                 \
+                [minute],               \
+                [second]
 
-                invoke  GetDlgItem,             \
-                        [hwnd],                 \
-                        ID_STATIC
+        invoke  GetDlgItem,             \
+                [hwnd],                 \
+                ID_STATIC
 
-                invoke  SetWindowText,          \
-                        eax,                    \
-                        time_text
+        invoke  SetWindowText,          \
+                eax,                    \
+                time_text
 
-                invoke  Sleep,1000
-                jmp     .loop
+        invoke  Sleep,1000
+        jmp     .loop
 
-                mov     esp,ebp
-                pop     ebp
-                ret
+        mov     esp,ebp
+        pop     ebp
+        ret
 
 proc DialogProc hwnddlg,msg,wparam,lparam
-                push    ebx esi edi
+        push    ebx esi edi
 
-                cmp     [msg],WM_INITDIALOG
-                je      .wminitdialog
-                cmp     [msg],WM_COMMAND
-                je      .wmcommand
-                cmp     [msg],WM_CLOSE
-                je      .wmclose
+        cmp     [msg],WM_INITDIALOG
+        je      .wminitdialog
+        cmp     [msg],WM_COMMAND
+        je      .wmcommand
+        cmp     [msg],WM_CLOSE
+        je      .wmclose
 
-                xor     eax,eax
-                jmp     .finish
+        xor     eax,eax
+        jmp     .finish
   .wmcommand:
-                cmp     [wparam],BN_CLICKED shl 16 + ID_START
-                je      .f_start
-                cmp     [wparam],BN_CLICKED shl 16 + ID_STOP
-                je      .f_stop
-                jmp     .exit
+        cmp     [wparam],BN_CLICKED shl 16 + ID_START
+        je      .f_start
+        cmp     [wparam],BN_CLICKED shl 16 + ID_STOP
+        je      .f_stop
+        jmp     .exit
   .f_start:
-                invoke  TerminateThread,        \
-                        [threadHandle],         \
-                        0
+        invoke  TerminateThread,        \
+                [threadHandle],         \
+                0
 
-                invoke  CreateThread,           \
-                        0,                      \
-                        0,                      \
-                        ThreadProc,             \
-                        0,                      \
-                        0,                      \
-                        threadID
+        invoke  CreateThread,           \
+                0,                      \
+                0,                      \
+                ThreadProc,             \
+                0,                      \
+                0,                      \
+                threadID
 
-                mov     [threadHandle],eax
-                jmp     .exit
+        mov     [threadHandle],eax
+        jmp     .exit
   .f_stop:
-                invoke  TerminateThread,        \
-                        [threadHandle],         \
-                        0
+        invoke  TerminateThread,        \
+                [threadHandle],         \
+                0
 
-                jmp     .exit
+        jmp     .exit
   .wminitdialog:
-                mov     eax,[hwnddlg]
-                mov     [hwnd],eax
-                jmp     .exit
+        mov     eax,[hwnddlg]
+        mov     [hwnd],eax
+        jmp     .exit
   .wmclose:
-                invoke  EndDialog,              \
-                        [hwnddlg],              \
-                        0
+        invoke  EndDialog,              \
+                [hwnddlg],              \
+                0
   .exit:
-                mov     eax,1
+        mov     eax,1
   .finish:
-                pop     edi esi ebx
-                ret
+        pop     edi esi ebx
+        ret
 endp
 
 .end start
