3f1192ef152ed88f5f3c225f940da46dc0ef71f6
slixenfeld <slixenfeld@mailbox.org>
2025-10-08 16:44:07 +0100

    Update stopwatch_dialog

--- stopwatch_dialog.asm
+++ stopwatch_dialog.asm
@@ -1,21 +1,18 @@
 ; Stopwatch Dialog
 
-format PE GUI 4.0
-
-include 'win32wx.inc'
+	format PE GUI 4.0
+	include 'win32wx.inc'
 
 	ID_STATIC= 50
 	ID_START = 200
 	ID_STOP  = 201
-
-	ID_TIMER = 1
 .data
 
 ; handle
 	hwnd	       dd	?
 
 ; timer
-	timer_text     rb	0xFF
+	time_text      rb	0xFF
 	second	       dd	0
 	minute	       dd	0
 	hour	       dd	0
@@ -27,7 +24,14 @@ include 'win32wx.inc'
 
 start:
 		invoke	GetModuleHandle,0
-		invoke	DialogBoxParam,eax,123,HWND_DESKTOP,DialogProc,0
+
+		invoke	DialogBoxParam, 	\
+			eax,			\
+			123,			\
+			HWND_DESKTOP,		\
+			DialogProc,		\
+			0
+
 		invoke	ExitProcess,0
 ThreadProc:
 		push	ebp
@@ -46,9 +50,20 @@ ThreadProc:
 		mov	[minute],0
 		inc	[hour]
   .skip:
-		invoke	wsprintf,timer_text,'%02i:%02i:%02i',[hour],[minute],[second]
-		invoke	GetDlgItem,[hwnd],ID_STATIC
-		invoke	SetWindowText,eax,timer_text
+		invoke	wsprintf,		\
+			time_text,		\
+			'%02i:%02i:%02i',	\
+			[hour], 		\
+			[minute],		\
+			[second]
+
+		invoke	GetDlgItem,		\
+			[hwnd], 		\
+			ID_STATIC
+
+		invoke	SetWindowText,		\
+			eax,			\
+			time_text
 
 		invoke	Sleep,1000
 		jmp	.loop
@@ -76,19 +91,34 @@ proc DialogProc hwnddlg,msg,wparam,lparam
 		je	.f_stop
 		jmp	.exit
   .f_start:
-		invoke	TerminateThread,[threadHandle],0
-		invoke	CreateThread,0,0,ThreadProc,0,0,threadID
+		invoke	TerminateThread,	\
+			[threadHandle], 	\
+			0
+
+		invoke	CreateThread,		\
+			0,			\
+			0,			\
+			ThreadProc,		\
+			0,			\
+			0,			\
+			threadID
+
 		mov	[threadHandle],eax
 		jmp	.exit
   .f_stop:
-		invoke	TerminateThread,[threadHandle],0
+		invoke	TerminateThread,	\
+			[threadHandle], 	\
+			0
+
 		jmp	.exit
   .wminitdialog:
 		mov	eax,[hwnddlg]
 		mov	[hwnd],eax
 		jmp	.exit
   .wmclose:
-		invoke	EndDialog,[hwnddlg],0
+		invoke	EndDialog,		\
+			[hwnddlg],		\
+			0
   .exit:
 		mov	eax,1
   .finish:
@@ -103,8 +133,8 @@ section '.rsrc' resource data readable
     directory RT_DIALOG,dialogs
     resource dialogs,123,LANG_ENGLISH+SUBLANG_DEFAULT,demonstration
 
-    dialog demonstration,   'Stopwatch' 	  ,250,250,130,50,	WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME
-	dialogitem 'STATIC','00:00:00',ID_STATIC  ,48,10,120,15,	WS_VISIBLE+WS_CHILD
-	dialogitem 'BUTTON','Start'   ,ID_START   ,5,30,50,15,		WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
-	dialogitem 'BUTTON','Stop'    ,ID_STOP	  ,70,30,50,15, 	WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
+    dialog demonstration,   'Stopwatch' 		      ,250,250,130,50,	    WS_CAPTION+WS_POPUP+WS_SYSMENU+DS_MODALFRAME
+	dialogitem 'STATIC','00:00:00'		  ,ID_STATIC  ,48,10,120,15,	    WS_VISIBLE+WS_CHILD
+	dialogitem 'BUTTON','Start'		  ,ID_START   ,5,30,50,15,	    WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
+	dialogitem 'BUTTON','Stop'		  ,ID_STOP    ,70,30,50,15,	    WS_VISIBLE+WS_TABSTOP+BS_DEFPUSHBUTTON
     enddialog
