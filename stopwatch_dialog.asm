4e71a986b099c821e58c829499bfe8e62a859241
slixenfeld <slixenfeld@mailbox.org>
2025-12-07 00:32:00 +0100

    stopwatch_dialog  - formatting

--- stopwatch_dialog.asm
+++ stopwatch_dialog.asm
@@ -1,129 +1,78 @@
 ; Stopwatch Dialog
 
-        format PE GUI 4.0
-        include 'win32wx.inc'
-; constants
-        ID_STATIC       =       50
-        ID_START        =       200
-        ID_STOP         =       201
+format PE GUI 4.0
+include 'win32wx.inc'
+
+                ID_STATIC       =       50
+                ID_START        =       200
+                ID_STOP         =       201
 .data
+                hwnd           dd       ?
 
-; handle
-        hwnd           dd       ?
+                time_text      rb       0xFF
+                second         dd       0
+                minute         dd       0
+                hour           dd       0
 
-; timer
-        time_text      rb       0xFF
-        second         dd       0
-        minute         dd       0
-        hour           dd       0
-
-; thread
-        threadID       dd       1
-        threadHandle   dd       0
+                threadID       dd       1
+                threadHandle   dd       0
 .code
 
-start:
-        invoke  GetModuleHandle,0
+start:          invoke  GetModuleHandle,0
+                invoke  DialogBoxParam,eax,123,HWND_DESKTOP,DialogProc,0
+                invoke  ExitProcess,0
 
-        invoke  DialogBoxParam,         \
-                eax,                    \
-                123,                    \
-                HWND_DESKTOP,           \
-                DialogProc,             \
-                0
-
-        invoke  ExitProcess,0
-ThreadProc:
-        push    ebp
-        mov     ebp,esp
-        mov     [second],-1
-        mov     [minute],0
-        mov     [hour],0
-  .loop:
-        inc     [second]
-        cmp     [second],60
-        jne     .skip
-        mov     [second],0
-        inc     [minute]
-        cmp     [minute],60
-        jne     .skip
-        mov     [minute],0
-        inc     [hour]
-  .skip:
-        invoke  wsprintf,               \
-                time_text,              \
-                '%02i:%02i:%02i',       \
-                [hour],                 \
-                [minute],               \
-                [second]
-
-        invoke  GetDlgItem,             \
-                [hwnd],                 \
-                ID_STATIC
-
-        invoke  SetWindowText,          \
-                eax,                    \
-                time_text
-
-        invoke  Sleep,1000
-        jmp     .loop
-
-        mov     esp,ebp
-        pop     ebp
-        ret
+ThreadProc:     push    ebp
+                mov     ebp,esp
+                mov     [second],-1
+                mov     [minute],0
+                mov     [hour],0
+.loop:          inc     [second]
+                cmp     [second],60
+                jne     .skip
+                mov     [second],0
+                inc     [minute]
+                cmp     [minute],60
+                jne     .skip
+                mov     [minute],0
+                inc     [hour]
+.skip:          invoke  wsprintf,time_text,'%02i:%02i:%02i',[hour],[minute],[second]
+                invoke  GetDlgItem,[hwnd],ID_STATIC
+                invoke  SetWindowText,eax,time_text
+                invoke  Sleep,1000
+                jmp     .loop
+                mov     esp,ebp
+                pop     ebp
+                ret
 
 proc DialogProc hwnddlg,msg,wparam,lparam
-        push    ebx esi edi
-
-        cmp     [msg],WM_INITDIALOG
-        je      .wminitdialog
-        cmp     [msg],WM_COMMAND
-        je      .wmcommand
-        cmp     [msg],WM_CLOSE
-        je      .wmclose
-
-        xor     eax,eax
-        jmp     .finish
-  .wmcommand:
-        cmp     [wparam],BN_CLICKED shl 16 + ID_START
-        je      .f_start
-        cmp     [wparam],BN_CLICKED shl 16 + ID_STOP
-        je      .f_stop
-        jmp     .exit
-  .f_start:
-        invoke  TerminateThread,        \
-                [threadHandle],         \
-                0
-
-        invoke  CreateThread,           \
-                0,                      \
-                0,                      \
-                ThreadProc,             \
-                0,                      \
-                0,                      \
-                threadID
-
-        mov     [threadHandle],eax
-        jmp     .exit
-  .f_stop:
-        invoke  TerminateThread,        \
-                [threadHandle],         \
-                0
-
-        jmp     .exit
-  .wminitdialog:
-        mov     eax,[hwnddlg]
-        mov     [hwnd],eax
-        jmp     .exit
-  .wmclose:
-        invoke  EndDialog,              \
-                [hwnddlg],              \
-                0
-  .exit:
-        mov     eax,1
-  .finish:
-        pop     edi esi ebx
-        ret
+                push    ebx esi edi
+                cmp     [msg],WM_INITDIALOG
+                je      .wminitdialog
+                cmp     [msg],WM_COMMAND
+                je      .wmcommand
+                cmp     [msg],WM_CLOSE
+                je      .wmclose
+                xor     eax,eax
+                jmp     .finish
+.wmcommand:     cmp     [wparam],BN_CLICKED shl 16 + ID_START
+                je      .f_start
+                cmp     [wparam],BN_CLICKED shl 16 + ID_STOP
+                je      .f_stop
+                jmp     .exit
+.f_start:       invoke  TerminateThread,[threadHandle],0
+                invoke  CreateThread,0,0,ThreadProc,0,0,threadID
+                mov     [threadHandle],eax
+                jmp     .exit
+.f_stop:        invoke  TerminateThread,[threadHandle],0
+                jmp     .exit
+.wminitdialog:  mov     eax,[hwnddlg]
+                mov     [hwnd],eax
+                jmp     .exit
+.wmclose:       invoke  EndDialog,[hwnddlg],0
+.exit:          mov     eax,1
+.finish:        pop     edi esi ebx
+                ret
 endp
 
 .end start
